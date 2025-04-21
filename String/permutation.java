999e50f4408f9f9fdc4ad55ecdb4d362006eeda0
vishesha841 <140616379+vishesha841@users.noreply.github.com>
2025-04-21 14:30:46 +0530

    Update permutation.java

--- String/permutation.java
+++ String/permutation.java
@@ -15,3 +15,24 @@ i/p
 abc     
 o/p
 a,ab,abc,ac,b,bc,c
+
+####
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {  
+            for (int j = i + 1; j <= n; j++) { 
+                for (int k = i; k < j; k++) {  
+                    System.out.print(s.charAt(k));
+                }
+                System.out.println(); 
+            }
+        }
+    }
+}
+
