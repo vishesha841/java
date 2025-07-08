f21a43497a814a3da323eb3eeb11601fa60ae4b6
vishesha841 <140616379+vishesha841@users.noreply.github.com>
2025-07-08 12:01:57 +0530

    Create ArrayList.java

---
+++ collection/ArrayList.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ public class Main{
+     public static void main(String args[]){
+      ArrayList <Integer> a = new ArrayList<>();
+      a.add(1);
+      a.add(2);
+      a.add(8);
+      a.add(9);
+      a.add(7);
+      a.add(12);
+      System.out.println("list: "+a);
+      System.out.println(a.size());
+      System.out.println(a.set(3,5));
+      System.out.println(a.get(3));
+       
+       
+     }
+ }
